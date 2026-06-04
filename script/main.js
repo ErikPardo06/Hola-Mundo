5f7c819338bca431f97970ca84258be73593cb48
Erik <erikpar13@gmail.com>
2026-06-04 15:27:12 +0200

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,23 @@
+const miTitulo = document.querySelector("h1");
+miTitulo.textContent = "¡Hola Mundo!";
+
+
+
+let helado = "Vainilla";
+
+if (helado === "Vainilla") {
+    alert("Me encanta el helado de Vainilla");
+} else {
+    alert("No me gusta el helado de Vainilla");
+}
+
+
+function multiplica(num1, num2) {
+    let resultado = num1 * num2;
+    return resultado;
+}
+
+
+multiplica(5, 10);
+multiplica(2, 3);
+multiplica(10, 10);
